638438c87e90568f9cc0f82ec750c33afc2ecbea
Joseph Ibochi <josephibochi@gmail.com>
2022-04-04 17:04:34 +0100

    added responsiveness for mobile

--- src/utility.js
+++ src/utility.js
@@ -3,18 +3,43 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const closeMenuBtn = document.querySelector(".close");
   const openMenuBtn = document.querySelector(".open");
-  console.log(openMenuBtn);
 
+  if (document.readyState === "complete") {
+    
+    const nav = document.querySelectorAll(".nav__column");
+    nav.addEventListener('click', () => {
+        categoryBtn.addEventListener("click", this.navcloseHandler.bind(this));
+
+    })
+  }
   openMenuBtn.addEventListener("click", () => {
     if (document.readyState === "complete") {
       const navMenu = document.querySelector(".nav__column");
-
-      navMenu.style.display = "block";
-      openMenuBtn.style.opacity = 0;
       if (navMenu.classList.contains("active")) {
         return;
       }
       navMenu.classList.add("active");
+      openMenuBtn.style.display = "none";
+      closeMenuBtn.style.display = "block";
     }
-  });   
+  });
+
+  closeMenuBtn.addEventListener("click", this.navcloseHandler.bind(this));
 });
+
+function navcloseHandler() {
+  const closeMenuBtn = document.querySelector(".close");
+  const openMenuBtn = document.querySelector(".open");
+
+  if (document.readyState === "complete") {
+    const navMenu = document.querySelector(".nav__column");
+
+    if (navMenu.classList.contains("active")) {
+      navMenu.classList.remove("active");
+    } else {
+      return;
+    }
+    closeMenuBtn.style.display = "none";
+    openMenuBtn.style.display = "block";
+  }
+}
